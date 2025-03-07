21c50d3e3c88c689791e49a6d90e7ce721d50744
sangeethaMavencart <154861833+sangeethaMavencart@users.noreply.github.com>
2025-03-07 09:34:24 +0530

    fix: origin issue

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+module.exports = ({env}) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+        keys: env.array("APP_KEYS"),
+    },
+    webhooks: {
+        populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
+    settings: {
+        cors: {
+            enabled: true,
+            origin: ["https://website-cyaa7ixkm-websitestitchflowios-projects.vercel.app"],
+        },
+    },
 });
