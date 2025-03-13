d7b4aed96fe593cd65e163b96537c7b61adbb2f6
stitchflow-website <website@stitchflow.io>
2025-03-13 12:35:53 +0530

    media url done

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          url: env('PUBLIC_URL', 'https://content.stitchflow.com'),
+        },
+      },
+    },
+  },
+});
