ed154f63c987fe3cd28ae527fb1cf6750c8fa724
Mafaz0206 <mafaz@mavencart.com>
2025-03-22 06:45:17 +0530

    test: image url path settings

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,10 @@ module.exports = ({ env }) => ({
     upload: {
         config: {
             providerOptions: {
-                baseUrl: "https://content.stitchflow.com",
+                baseUrl: "https://content.stitchflow.com", // ✅ Ensure this is correct
+                localServer: {
+                    url: env("PUBLIC_URL", "https://content.stitchflow.com"), // ✅ Make sure this matches
+                },
             },
         },
     },
