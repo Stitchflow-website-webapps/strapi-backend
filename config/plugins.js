ac4a3dbf5f519010e9f47d2a0c68b6cdcfdd8725
Mafaz0206 <mafaz@mavencart.com>
2025-03-27 13:10:30 +0530

    terms/priacy component created

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,1 @@
-module.exports = ({ env }) => ({
-    upload: {
-        config: {
-            providerOptions: {
-                baseUrl: env("https://content.stitchflow.com", "https://content.stitchflow.com"),
-                localServer: {
-                    url: env("https://content.stitchflow.com", "https://content.stitchflow.com"),
-                },
-            },
-        },
-    },
-});  
+module.exports = () => ({});
